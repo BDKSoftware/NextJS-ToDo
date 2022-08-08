46ebc534cb20086224ee00fcdeb94134e30b2ec4
Bradley Donn Kukuk <brad@bdksoftware.com>
2022-08-08 12:36:32 -0600

    Changes So no zoom on input 2

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Login() {
         <title>NextJS Todo Login</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        ></meta>
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
         <meta name="description" content="ToDo Practice Project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
